9f1944ac21a71f58886d1139d0ae15363906a055
apple6356 <apple1418@naver.com>
2024-03-09 23:25:23 +0900

    24-03-09

---
+++ src/main/java/com/Project/Board/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.Project.Board.controller;
+
+import com.Project.Board.domain.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setUserId(form.getUserId());
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/Project/Board/domain/Member.java
@@ -0,0 +1,40 @@
+package com.Project.Board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
